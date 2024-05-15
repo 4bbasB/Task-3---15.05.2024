0975f3b10517033c1c0bba445b68cd26401c8ffb
4bbasB <abbasbayramli1719@gmail.com>
2024-05-15 12:08:44 +0400

    Done

---
+++ Task 3 - 15.05.2024/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_3___15._05._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Verilmiş ededler siyahisindaki en böyük ededi tapan alqoritm
+            2. Verilmiş Products arrayindəki Product objectlərinin Id'ləri tək olanlarının 
+            Price'larının ədədi ortasını tapan algorithm yazın.
+            (Product objectinin propertyləri: Id(yeni reqem ve ya eded), Name, Price, StockCount)*/
+
+            #region Task 1
+            //int[] numbers = { 20, -199, 77, 11, 0, -79, 23, 18 };
+            //int temp = 0;
+            //foreach (int number in numbers)
+            //{
+            //    if (number > temp)
+            //        temp = number;
+            //}
+            //Console.WriteLine(temp);
+            #endregion
+
+            #region Task 2
+            //var ProductObj1 = new {Id = 1, Name = "Samsung", Price = 2400, StockCount = 1};
+            //var ProductObj2 = new { Id = 2, Name = "Iphone", Price = 3000, StockCount = 15 };
+            //var ProductObj3 = new { Id = 3, Name = "Xiaomi", Price = 259, StockCount = 20 };
+            //var ProductObj4 = new { Id = 4, Name = "Huawei", Price = 509, StockCount = 5 };
+            //var ProductObj5 = new { Id = 5, Name = "Oppo", Price = 1230, StockCount = 3 };
+            //var ProductObj6 = new { Id = 6, Name = "Lenovo", Price = 1000, StockCount = 19 };
+
+            //int sum = 0;
+
+            //var Products = new[] { ProductObj1, ProductObj2, ProductObj3, ProductObj4, ProductObj5, ProductObj6 };
+            //foreach (var Product in Products)
+            //{
+            //    if(Product.Id % 2 == 1)
+            //        sum += Product.Price;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
